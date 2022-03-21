8860c9123f5cb62b568c1b23da452a856e349e19
Cody Jarrett <35110401+codymjarrett@users.noreply.github.com>
2022-03-21 00:08:21 -0500

    add how it works section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,17 @@
 import {
   AspectRatio,
+  Badge,
   Box,
+  Center,
   Container,
   Flex,
   Heading,
   Text,
-  useMediaQuery,
+  ListItem,
+  OrderedList,
 } from '@chakra-ui/react';
+import { ArrowLeftIcon, ArrowRightIcon } from '@chakra-ui/icons';
+
 import type { NextPage } from 'next';
 import Layout from '../components/Layout';
 
@@ -14,6 +19,12 @@ interface YouTubeVideoProps {
   embedId: String;
   title: string;
 }
+
+const items = [
+  'Sit the trash out at the contractual times. (Typically after 5pm).',
+  'Our uniformed Valet Trashman will then pickup disposables and drop them off at the nearest trash room/chute/dumpster.',
+  'Residents will then collect their bins from the door each pickup night (Typically 9pm).',
+];
 
 const YouTubeVideo = ({ embedId, title }: YouTubeVideoProps) => {
   return (
@@ -56,11 +67,39 @@ const Home: NextPage = () => {
           This is a PERFECT amenity for your community. We pride ourselves on
           delivering your disposables, with the care they don’t deserve.
         </Text>
-        {/* <Flex> */}
         <AspectRatio maxW="560px" maxH="315px" mt={10} ml="auto" mr="auto">
           <YouTubeVideo embedId="XVZCiNCn3dk" title="Butler's Trash Removal" />
         </AspectRatio>
-        {/* </Flex> */}
+        <Box mt={10}>
+          <Heading textAlign="center">How It Works</Heading>
+          <Text mt={3} fontSize={{ base: 'md', md: 'xl' }} textAlign="center">
+            Butler’s Trash Valet Service is very seamleass and easy. Residents
+            are given designated times for trash set out and pickup,
+            customizable to your community. Enjoy doorside trash removal 3, 5,
+            or 7 days a week.
+          </Text>
+          <Text fontSize={{ base: 'md', md: 'xl' }} mt={4}>
+            Your residents will:
+          </Text>
+          <OrderedList mt={2}>
+            {items.map((item, index) => (
+              <ListItem key={index}>
+                <Text fontSize={{ base: 'md', md: 'xl' }}>{item}</Text>
+              </ListItem>
+            ))}
+          </OrderedList>
+        </Box>
+        <Center my={10}>
+          <Badge fontSize="xl" p={5} colorScheme="red">
+            <Text fontSize="sm">
+              ASK ABOUT OUR KNOCK & GO SERVICE <ArrowRightIcon ml={4} />
+            </Text>
+            <Text fontSize="sm" mt={5}>
+              <ArrowLeftIcon mr={4} />
+              Easy, Convienient and Pain-free
+            </Text>
+          </Badge>
+        </Center>
       </Container>
     </Layout>
   );
